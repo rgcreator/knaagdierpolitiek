c97d4740be5a9dc34e1f2ecdd334a7992c0d8f6a
rgcreator <ricardovg35@gmail.com>
2023-07-27 19:21:46 +0200

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,17 +1,32 @@
 // Description: This file contains the javascript code for the style of the website.
 function zoeken () {
-var search = document.getElementById("search").value;
+var search = document.getElementById("search").value.toLowerCase()
 switch (search) {
 case "eekhoorn":
    document.getElementById("wilhout").style.display = "none";
    document.getElementById("mar").style.display = "block";
-
    display = "eekhoorn";
    break;
+   case "Marijnissen":
+   document.getElementById("wilhout").style.display = "none";
+   document.getElementById("mar").style.display = "block";
+   display = "eekhoorn";
+   break;
+   case "Eekhoorn Marijnissen":
+    document.getElementById("wilhout").style.display = "none";
+    document.getElementById("mar").style.display = "block";
+    display = "eekhoorn";
+    break;
+    case "SP":
+        document.getElementById("wilhout").style.display = "none";
+        document.getElementById("mar").style.display = "block";
+        display = "eekhoorn";
+        break;
 case "wilhout":
     document.getElementById("mar").style.display = "none";
     document.getElementById("wilhout").style.display = "block";
     display = "wilhout";
     break;
+    
 }
 }
