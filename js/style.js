cc429acd9ee0833842ecfbb3733262acd3104441
rgcreator <ricardovg35@gmail.com>
2023-07-28 19:42:09 +0200

    update

--- js/style.js
+++ js/style.js
@@ -1,48 +1,16 @@
 // Description: This file contains the javascript code for the style of the website.
 function zoeken () {
 var search = document.getElementById("search").value.toLowerCase()
-switch (search) {
-case "eekhoorn":
+ if ((search == 'eekhoorn') || (search == 'marijnissen') || ((search == 'eekhoorn marijnissen') || (search == 'sp') ) ){
+
    document.getElementById("wilhout").style.display = "none";
    document.getElementById("mar").style.display = "block";
-   display = "eekhoorn";
-   break;
-   case "marijnissen":
-   document.getElementById("wilhout").style.display = "none";
-   document.getElementById("mar").style.display = "block";
-   display = "eekhoorn";
-   break;
-   case "eekhoorn marijnissen":
-    document.getElementById("wilhout").style.display = "none";
-    document.getElementById("mar").style.display = "block";
-    display = "eekhoorn";
-    break;
-    case "sp":
-        document.getElementById("wilhout").style.display = "none";
-        document.getElementById("mar").style.display = "block";
-        display = "eekhoorn";
-        break;
-case "wilhout":
-    document.getElementById("mar").style.display = "none";
+   display = "Eekhoorn Marijnissen";
+ }
+ if ((search == 'wilhout') || (search == 'geert') || ((search == 'geert wilhout') || (search == 'pvv') ) ){
+
     document.getElementById("wilhout").style.display = "block";
-    display = "wilhout";
-    break;
-    
-case "geert wilhout":
     document.getElementById("mar").style.display = "none";
-    document.getElementById("wilhout").style.display = "block";
-    display = "wilhout";
-    break;
-    case "geert":
-    document.getElementById("mar").style.display = "none";
-    document.getElementById("wilhout").style.display = "block";
-    display = "wilhout";
-    break;
-    case "pvv":
-    document.getElementById("mar").style.display = "none";
-    document.getElementById("wilhout").style.display = "block";
-    display = "wilhout";
-    break;
-    
-}
+    display = "Geert Wilhout";
+  }
 }
